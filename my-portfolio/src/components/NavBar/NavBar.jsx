d76fc861dd15365d1ec1b968b1ec14839af7c403
Kevelaz <kevinvelazquez925@gmail.com>
2024-02-18 22:45:24 -0500

    add framework for portfolio website

---
+++ my-portfolio/src/components/NavBar/NavBar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Navigate } from "react-router-dom";
+
+const NavBar = () => {
+  return (
+    <div>
+      {/* <Link Navigate="/about">About Me</Link> */}
+    </div>
+  )
+}
+
+export default NavBar 

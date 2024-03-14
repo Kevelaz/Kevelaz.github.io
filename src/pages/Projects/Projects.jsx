9bc9e6d648ff8b653cf6c293acdeedf703d1f7ce
Kevelaz <kevinvelazquez925@gmail.com>
2024-03-14 18:01:06 -0400

    add font

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -3,11 +3,14 @@ import React from "react";
 const Projects = () => {
   return (
     <div className="space-y-8"> {/* Adds vertical spacing between each project */}
+      <div className=""> 
+        <h1>Projects</h1> 
+        </div>
       <div className="flex flex-col md:flex-row items-center justify-between space-y-4 md:space-y-0   md:space-x-4 bg-gray-100 p-6 rounded-lg shadow-md">
         <img src="src/assets/img/thumbnail-for-EfT.jpg" alt="Project" className="w-full md:w-1/4 rounded-lg" />
         <div className="w-full md:w-2/3">
           <h2 className="text-4xl  text-custom-EFT font-tilt">Escape From Tenochtitlan</h2>
-          <p className="text-gray-700">Description here</p>
+          <p className="text-gray-700">'Escape from Tenochtitlan' is a thrilling choice-based adventure game set in the heart of the Aztec Empire. Players take on the role of a prisoner destined for sacrifice, who must navigate through a maze of paths, make strategic decisions, and solve puzzles based on Aztec culture to escape death. Each choice directly impacts the storyline, offering a unique blend of historical intrigue and immersive gameplay. Stealth, strategy, and ethical dilemmas enrich the experience, challenging players to not only survive but also confront moral questions on their journey to freedom. Dive into an ancient world where every decision could be your last in this captivating escape adventure.</p>
           <div className="flex space-x-4 mt-4">
             <a href="https://escape-from-tenochtitlan.netlify.app/" target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:text-blue-700">Live Demo</a>
             <a href="https://github.com/Kevelaz/Escape-from-Tenochtitlan" target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:text-blue-700">GitHub Repo</a>
@@ -29,7 +32,7 @@ const Projects = () => {
       <div className="flex flex-col md:flex-row items-center justify-between space-y-4 md:space-y-0   md:space-x-4 bg-gray-100 p-6 rounded-lg shadow-md">
         <img src="src/assets/img/thumbnail-for-TG.jpg" alt="Project" className="w-full md:w-1/4 rounded-lg" />
         <div className="w-full md:w-2/3">
-          <h2 className="text-6xl font-italianno">Travel Gold</h2>
+          <h2 className="text-6xl text-gold font-italianno">Travel Gold</h2>
           <p className="text-gray-700">Description here</p>
           <div className="flex space-x-4 mt-4">
             <a href="https://peaceful-pasca-8105c8.netlify.app/" target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:text-blue-700">Live Demo</a>

131b29b3b541f90aeb6f2b0be4fc1e3565471856
Kevelaz <kevinvelazquez925@gmail.com>
2024-03-19 18:36:50 -0400

    add basic structure to homepage

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,11 +4,11 @@ import './Home.css'
 const Home = () => {
   return (
     <div>
-      <h1 className="Name">Welcome</h1>
-      <h2 className="Description">
-        My name is Kevin Medina Velazquez and im a junior software developer. As a first generation mexican american, I inhereted the values and morals of hardwork leads to better outcomes and treat everyone with the same respect. 
-        As a junior software developer, Im learning and growing with each and every project i embark on, my biggest acheivement thus far is completing a website for a local small business named Baja Mobile Mechanic. It was my first endeavor into the tech space after graduating from the 3-month long, software engineering program at General Assembly. 
-      </h2>
+      <h1 className="">Welcome</h1>
+      <h2 className="Description">My name is Kevin Velazquez</h2>
+      <div id="links-for-websites">
+            <a></a>
+      </div>
     </div>
   )
 }

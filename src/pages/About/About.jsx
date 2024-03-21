a542a3ffa5769b3816cb29216f463bdcc8106399
Kevelaz <kevinvelazquez925@gmail.com>
2024-03-21 17:52:53 -0400

    add some additonal styling

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -2,11 +2,12 @@ import React from "react";
 
 const About = () => {
   return (
-    <div>
-      <h1 className="relative z-10">
+    <div className=" relative z-10">
+      <h1 className="text-2xl text-white text-center">
                 My name is Kevin Medina Velazquez and im a junior software developer. As a first generation mexican american, I inhereted the values and morals of hardwork leads to better outcomes and treat everyone with the same respect. 
                 As a junior software developer, Im learning and growing with each and every project i embark on, my biggest acheivement thus far is completing a website for a local small business named Baja Mobile Mechanic. It was my first endeavor into the tech space after graduating from the 3-month long, software engineering program at General Assembly.
       </h1>
+      <img src="src/assets/img/IMG_3384 2.jpg" alt="Kevin Velazquez" className="rounded-full     w-96 h-96 object-cover object-center ml-36 scale-100" />
     </div>
   )
 }

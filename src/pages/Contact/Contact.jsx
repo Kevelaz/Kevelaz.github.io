eab5a4ee7f54ca500674e62a4f131bcff0a8d2ae
Kevelaz <kevinvelazquez925@gmail.com>
2024-04-01 16:19:52 -0400

    add framework for page

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -3,7 +3,10 @@ import React from "react";
 const Contact = () => {
   return (
     <div>
-        <h1 className="text-2xl relative z-10 text-slate-300 text-center"> LOL </h1>
+        <h1 className="text-2xl relative z-10 text-slate-300 text-center"> Phone Number : </h1>
+        <h1 className="text-2xl relative z-10 text-slate-300 text-center"> Email : </h1>
+        <h1 className="text-2xl relative z-10 text-slate-300 text-center"> Links: </h1>
+        <h1 className="text-2xl relative z-10 text-slate-300 text-center">  Input Info: </h1>
     </div>
   )
 }
